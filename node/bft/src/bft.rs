b6ff59028fc1f213d50163c11488e02fadcca76d
Kai Mast <kai@kaimast.com>
2025-12-03 14:01:11 -0800

    fix(bft/sync): pause bft block advancement during sync

--- node/bft/src/bft.rs
+++ node/bft/src/bft.rs
@@ -298,22 +298,20 @@ impl<N: Network> SyncCallback<N> for BFT<N> {
     /// This method commits all the certificates into the DAG.
     /// Note that there is no need to insert the certificates into the DAG, because these certificates
     /// already exist in the ledger and therefore do not need to be re-ordered into future committed subdags.
-    async fn sync_dag_at_bootup(&self, certificates: Vec<BatchCertificate<N>>) -> Result<()> {
+    fn sync_bft_dag_at_bootup(&self, certificates: &[BatchCertificate<N>]) {
         // Acquire the BFT write lock.
         let mut dag = self.dag.write();
 
         // Commit all the certificates.
         for certificate in certificates {
-            dag.commit(&certificate, self.storage().max_gc_rounds());
+            dag.commit(certificate, self.storage().max_gc_rounds());
         }
-
-        Ok(())
     }
 
-    /// Notification about a new certificate detected by the `Sync` instance after fetching a new block.
+    // Notification about a new certificate detected by the `Sync` instance after fetching a new block.
     async fn add_new_certificate(&self, certificate: BatchCertificate<N>) -> Result<()> {
         // Update the DAG with the certificate.
-        self.update_dag::<true>(certificate).await
+        self.update_dag::<false>(certificate).await
     }
 }
 
@@ -513,6 +511,8 @@ impl<N: Network> BFT<N> {
 
 impl<N: Network> BFT<N> {
     /// Stores the certificate in the DAG, and attempts to commit one or more anchors.
+    ///
+    /// Callers of this function are required to hold the write lock to the `LedgerService`, by calling [`Self::pause_for_block_sync`].
     async fn update_dag<const IS_SYNCING: bool>(&self, certificate: BatchCertificate<N>) -> Result<()> {
         // ### First, insert the certificate into the DAG. ###
         // Retrieve the round of the new certificate to add to the DAG.
@@ -757,7 +757,7 @@ impl<N: Network> BFT<N> {
                     consensus_sender.tx_consensus_subdag.send((subdag, transmissions, callback_sender)).await?;
                     // Await the callback to continue.
                     match callback_receiver.await {
-                        Ok(Ok(())) => (), // continue
+                        Ok(Ok(_)) => (), // continue
                         Ok(Err(err)) => {
                             let err = err.context(format!("BFT failed to advance the subdag for round {anchor_round}"));
                             error!("{}", &flatten_error(err));
@@ -910,9 +910,7 @@ impl<N: Network> BFT<N> {
         }
         Ok(traversal.contains(&previous_certificate))
     }
-}
 
-impl<N: Network> BFT<N> {
     /// Shuts down the BFT.
     pub async fn shut_down(&self) {
         info!("Shutting down the BFT...");
@@ -1527,7 +1525,7 @@ mod tests {
         let bootup_bft = initialize_bft(account.clone(), storage_2, ledger)?;
 
         // Sync the BFT DAG at bootup.
-        bootup_bft.sync_dag_at_bootup(certificates.clone()).await.unwrap();
+        bootup_bft.sync_bft_dag_at_bootup(&certificates);
 
         // Check that the BFT starts from the same last committed round.
         assert_eq!(bft.dag.read().last_committed_round(), bootup_bft.dag.read().last_committed_round());
@@ -1706,7 +1704,7 @@ mod tests {
         let bootup_bft = initialize_bft(account.clone(), bootup_storage.clone(), ledger.clone())?;
 
         // Sync the BFT DAG at bootup.
-        bootup_bft.sync_dag_at_bootup(pre_shutdown_certificates.clone()).await.unwrap();
+        bootup_bft.sync_bft_dag_at_bootup(&pre_shutdown_certificates);
 
         // Insert the post shutdown certificates to the storage and BFT with bootup.
         for certificate in post_shutdown_certificates.iter() {
@@ -1886,7 +1884,7 @@ mod tests {
         // Insert a mock DAG in the BFT without bootup.
         *bootup_bft.dag.write() = crate::helpers::dag::test_helpers::mock_dag_with_modified_last_committed_round(0);
         // Sync the BFT DAG at bootup.
-        bootup_bft.sync_dag_at_bootup(pre_shutdown_certificates.clone()).await.unwrap();
+        bootup_bft.sync_bft_dag_at_bootup(&pre_shutdown_certificates);
 
         // Insert the post shutdown certificates into the storage.
         let mut post_shutdown_certificates: Vec<snarkvm::ledger::narwhal::BatchCertificate<CurrentNetwork>> =
